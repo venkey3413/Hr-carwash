7e105e94f10185823815a457fda43c46714b4424
venkey3413 <144769456+venkey3413@users.noreply.github.com>
2025-01-02 10:18:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,19 @@ def home():
 
 @app.route('/add', methods=['POST'])
 def add_task():
-    task_name = request.form.get('task_name')
-    if task_name:
-        tasks.append(task_name)
+    user_name = request.form.get('user_name')
+    car_type = request.form.get('car_type')
+    washing_price = request.form.get('washing_price')
+    estimation_time = request.form.get('estimation_time')
+
+    if user_name and car_type and washing_price and estimation_time:
+        task = {
+            'user_name': user_name,
+            'car_type': car_type,
+            'washing_price': float(washing_price),
+            'estimation_time': int(estimation_time)
+        }
+        tasks.append(task)
     return redirect(url_for('home'))
 
 @app.route('/delete/<int:task_id>')
